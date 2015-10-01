a3052eaeb298705bec10c05aac21130b94870ea8
Gwen-Pigat <pixofheaven@gmail.com>
2015-10-01 15:09:09 +0200

    MAj vers POO + fetch dans BDD

--- include/section_videos.php
+++ include/section_videos.php
@@ -1,25 +1,20 @@
-<div class="container">
+<?php 
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section_videos">
-	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<h4>LIVE au "Gibus" (Paris)</h4>
-		<iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="https://www.youtube.com/embed/3P3N7nL5-Pc" frameborder="0" allowfullscreen></iframe>
-	</div>
-	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<h4>LIVE à Abondant</h4>
-		<iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="https://www.youtube.com/embed/1H_vQRgmJwM" frameborder="0" allowfullscreen></iframe>
-	</div>
-	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<h4>LIVE Emergenza</h4>
-		<iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="https://www.youtube.com/embed/ESos-RUOzN4" frameborder="0" allowfullscreen></iframe>
-	</div>
-	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<h4>Dedicated to slavery [BASS PLAYTHROUGH]</h4>
-		<iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="https://www.youtube.com/embed/6pXLTuZLPbc" frameborder="0" allowfullscreen></iframe>
-	</div>
-	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<h4>LIVE "La belle Rouge"</h4>
-		<iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="https://www.youtube.com/embed/y2vH98tzp_Y" frameborder="0" allowfullscreen></iframe>
-	</div>
+$content_videos = new Content();
 
-</div>
+echo Content::setOpenTag("div","container"); 
+
+	echo $content_videos->setopenTag("div","col-lg-12 col-md-12 col-sm-12 col-xs-12 section_videos");
+
+	$query = $link->query("SELECT * FROM Videos ORDER BY id DESC");
+
+	while ($row = $query->fetch_object()) {
+	echo $content_videos->setOpenTag("div","col-lg-6 col-md-6 col-sm-12 col-xs-12");
+		echo $content_videos->setContent("$row->title","h4");
+		echo $content_videos->setFrame("$row->href","col-lg-12 col-md-12 col-sm-12 col-xs-12",0,1);
+	echo $content_videos->setCloseTag("div");
+	}
+
+	echo $content_videos->setCloseTag("div");
+
+echo Content::setCloseTag("div"); ?>
